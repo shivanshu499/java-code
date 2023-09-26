3fffd9ec9ad976b65d8f8e9f409d41c17a69b8bc
shivanshu499 <142155602+shivanshu499@users.noreply.github.com>
2023-09-26 08:08:48 +0530

    Update and rename AlphabetCheck.java to Alphabet.java

--- Alphabet.java
+++ Alphabet.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AlphabetCheck {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
